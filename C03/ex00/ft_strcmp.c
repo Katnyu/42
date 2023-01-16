0282527432e668131c7773106faefc58edcec274
kiki <geoffreymartelin@gmail.com>
2023-01-16 18:04:00 +0100

    modification ex00

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,21 +6,16 @@
 /*   By: geoffrey <geoffreymartelin@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 10:08:36 by geoffrey          #+#    #+#             */
-/*   Updated: 2022/12/23 10:48:42 by geoffrey         ###   ########.fr       */
+/*   Updated: 2023/01/11 10:26:26 by geoffrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	result;
+	int	i;
 
-	while (*s1 != '\0' && *s1 == *s2)
-	{
-		result = *s1 - *s2;
-		if (result != 0)
-			return (result);
-		*s1++;
-		*s2++;
-	}
-	return (result);
-}	
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		++i;
+	return (s1[i] - s2[i]);
+}
